408815247d0511f85b3a23085c3b0a6caad03d96
MayfeelYang <mayfeelyang@gmail.com>
2016-01-05 21:48:20 -0800

    add recommend & add want_join

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20151231013907) do
-=======
 ActiveRecord::Schema.define(version: 20160104125054) do
 
   create_table "activities", force: :cascade do |t|
@@ -31,7 +28,6 @@ ActiveRecord::Schema.define(version: 20160104125054) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
   end
->>>>>>> origin/master
 
   create_table "admins", force: :cascade do |t|
     t.string   "name"
