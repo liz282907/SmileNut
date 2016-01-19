5bd78e871546cdc20d2321df95c6a27b984e46ed
MayfeelYang <mayfeelyang@gmail.com>
2016-01-19 08:18:24 +0000

    fix migrate 3

--- db/20160106101117_add_time_for_activity.rb
+++
@@ -1,6 +0,0 @@
-class AddTimeForActivity < ActiveRecord::Migration
-  def change
-    add_column :activities,:start_time,:time
-    add_column :activities,:end_time,:time
-  end
-end
--- db/20160106133224_change_type.rb
+++
@@ -1,7 +0,0 @@
-class ChangeType < ActiveRecord::Migration
-  def change
-    add_column :activities,:start_time,:datetime
-    add_column :activities,:end_time,:datetime
- 
-  end
-end
