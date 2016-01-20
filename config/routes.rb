a9b424cfa4b846ef7904d8e25fe7fd78689c2e1d
MayfeelYang <mayfeelyang@gmail.com>
2016-01-20 05:02:09 +0000

    new_merge fix logout and others

--- config/routes.rb
+++ config/routes.rb
@@ -46,8 +46,8 @@ end
   
   get 'login' => 'sessions#new' 
   post 'login' => 'sessions#create' 
-  delete 'logout' => 'sessions#destroy'
-
+  #delete 'logout' => 'sessions#destroy'
+  get 'logout' => 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
